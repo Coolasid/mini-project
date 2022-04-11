d3a20f93e7d735e65cd3c1d53fc268087ad13e0a
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-04-11 06:01:22 -0700

    completed navbar

---
+++ index.js
@@ -0,0 +1,34 @@
+
+// dummy database
+
+let products = [
+  {
+    image: './images/IMG_20220411_162917-removebg-preview.png',
+    advantage:
+      'Pramay- N P K 19:19:19 - Instant uptake of nutrients by the plants resulting in signigicant and quick improvement in crop will be seen.',
+    composition1: 'Total Nitrogen by weight      :  19%   Min.',
+    composition2: 'Nitrate Nitrogen by weight    :  4%    Max.',
+    composition3: 'AmmoniaCal Nitrogen by weight :  4.5%  Min.',
+    composition4: 'Urea Nitrogen by weight       :  10.5% Max.',
+    composition5: 'WS Phosphate by weight        :  19%   Min.',
+    composition6: 'WS potash by weight           :  19%   Min.',
+  },
+
+  {
+    image: './images/IMG_20220411_162845-removebg-preview.png',
+    advantage:
+      'Pramay- N P K 19:19:19 - Instant uptake of nutrients by the plants resulting in signigicant and quick improvement in crop will be seen.',
+    composition1: 'Total Nitrogen by weight      :  19%   Min.',
+    composition2: 'Nitrate Nitrogen by weight    :  4%    Max.',
+    composition3: 'AmmoniaCal Nitrogen by weight :  4.5%  Min.',
+    composition4: 'Urea Nitrogen by weight       :  10.5% Max.',
+    composition5: 'WS Phosphate by weight        :  19%   Min.',
+    composition6: 'WS potash by weight           :  19%   Min.',
+  },
+];
+
+let main = document.getElementById("main")
+
+
+
+
