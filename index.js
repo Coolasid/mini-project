2b6b894e37dcb1a5364bb4830f117a5b6128746a
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-04-12 11:19:34 +0530

    added products

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // dummy database
 
 let products = [
@@ -25,20 +24,188 @@ let products = [
     composition5: 'WS Phosphate by weight        :  19%   Min.',
     composition6: 'WS potash by weight           :  19%   Min.',
   },
+
+  {
+    image: './images/IMG_20220411_162856-removebg-preview.png',
+    advantage:
+      'Pramay- N P K 19:19:19 - Instant uptake of nutrients by the plants resulting in signigicant and quick improvement in crop will be seen.',
+    composition1: 'Total Nitrogen by weight      :  19%   Min.',
+    composition2: 'Nitrate Nitrogen by weight    :  4%    Max.',
+    composition3: 'AmmoniaCal Nitrogen by weight :  4.5%  Min.',
+    composition4: 'Urea Nitrogen by weight       :  10.5% Max.',
+    composition5: 'WS Phosphate by weight        :  19%   Min.',
+    composition6: 'WS potash by weight           :  19%   Min.',
+  },
+
+  {
+    image: './images/IMG_20220411_162906-removebg-preview.png',
+    advantage:
+      'Pramay- N P K 19:19:19 - Instant uptake of nutrients by the plants resulting in signigicant and quick improvement in crop will be seen.',
+    composition1: 'Total Nitrogen by weight      :  19%   Min.',
+    composition2: 'Nitrate Nitrogen by weight    :  4%    Max.',
+    composition3: 'AmmoniaCal Nitrogen by weight :  4.5%  Min.',
+    composition4: 'Urea Nitrogen by weight       :  10.5% Max.',
+    composition5: 'WS Phosphate by weight        :  19%   Min.',
+    composition6: 'WS potash by weight           :  19%   Min.',
+  },
+
+  {
+    image: './images/IMG_20220411_162912-removebg-preview.png',
+    advantage:
+      'Pramay- N P K 19:19:19 - Instant uptake of nutrients by the plants resulting in signigicant and quick improvement in crop will be seen.',
+    composition1: 'Total Nitrogen by weight      :  19%   Min.',
+    composition2: 'Nitrate Nitrogen by weight    :  4%    Max.',
+    composition3: 'AmmoniaCal Nitrogen by weight :  4.5%  Min.',
+    composition4: 'Urea Nitrogen by weight       :  10.5% Max.',
+    composition5: 'WS Phosphate by weight        :  19%   Min.',
+    composition6: 'WS potash by weight           :  19%   Min.',
+  },
+
+  {
+    image: './images/IMG_20220411_162917-removebg-preview (1).png',
+    advantage:
+      'Pramay- N P K 19:19:19 - Instant uptake of nutrients by the plants resulting in signigicant and quick improvement in crop will be seen.',
+    composition1: 'Total Nitrogen by weight      :  19%   Min.',
+    composition2: 'Nitrate Nitrogen by weight    :  4%    Max.',
+    composition3: 'AmmoniaCal Nitrogen by weight :  4.5%  Min.',
+    composition4: 'Urea Nitrogen by weight       :  10.5% Max.',
+    composition5: 'WS Phosphate by weight        :  19%   Min.',
+    composition6: 'WS potash by weight           :  19%   Min.',
+  },
+
+  {
+    image: './images/IMG_20220411_162928-removebg-preview.png',
+    advantage:
+      'Pramay- N P K 19:19:19 - Instant uptake of nutrients by the plants resulting in signigicant and quick improvement in crop will be seen.',
+    composition1: 'Total Nitrogen by weight      :  19%   Min.',
+    composition2: 'Nitrate Nitrogen by weight    :  4%    Max.',
+    composition3: 'AmmoniaCal Nitrogen by weight :  4.5%  Min.',
+    composition4: 'Urea Nitrogen by weight       :  10.5% Max.',
+    composition5: 'WS Phosphate by weight        :  19%   Min.',
+    composition6: 'WS potash by weight           :  19%   Min.',
+  },
+
+  {
+    image: './images/IMG_20220411_162936-removebg-preview.png',
+    advantage:
+      'Pramay- N P K 19:19:19 - Instant uptake of nutrients by the plants resulting in signigicant and quick improvement in crop will be seen.',
+    composition1: 'Total Nitrogen by weight      :  19%   Min.',
+    composition2: 'Nitrate Nitrogen by weight    :  4%    Max.',
+    composition3: 'AmmoniaCal Nitrogen by weight :  4.5%  Min.',
+    composition4: 'Urea Nitrogen by weight       :  10.5% Max.',
+    composition5: 'WS Phosphate by weight        :  19%   Min.',
+    composition6: 'WS potash by weight           :  19%   Min.',
+  },
+  {
+    image: './images/IMG_20220411_162942-removebg-preview.png',
+    advantage:
+      'Pramay- N P K 19:19:19 - Instant uptake of nutrients by the plants resulting in signigicant and quick improvement in crop will be seen.',
+    composition1: 'Total Nitrogen by weight      :  19%   Min.',
+    composition2: 'Nitrate Nitrogen by weight    :  4%    Max.',
+    composition3: 'AmmoniaCal Nitrogen by weight :  4.5%  Min.',
+    composition4: 'Urea Nitrogen by weight       :  10.5% Max.',
+    composition5: 'WS Phosphate by weight        :  19%   Min.',
+    composition6: 'WS potash by weight           :  19%   Min.',
+  },
+  {
+    image: './images/IMG_20220411_162956-removebg-preview.png',
+    advantage:
+      'Pramay- N P K 19:19:19 - Instant uptake of nutrients by the plants resulting in signigicant and quick improvement in crop will be seen.',
+    composition1: 'Total Nitrogen by weight      :  19%   Min.',
+    composition2: 'Nitrate Nitrogen by weight    :  4%    Max.',
+    composition3: 'AmmoniaCal Nitrogen by weight :  4.5%  Min.',
+    composition4: 'Urea Nitrogen by weight       :  10.5% Max.',
+    composition5: 'WS Phosphate by weight        :  19%   Min.',
+    composition6: 'WS potash by weight           :  19%   Min.',
+  },
 ];
 
-let main = document.getElementById("main")
+let main = document.getElementById('main');
 
-products.forEach((product)=>{
+products.forEach((product) => {
+  let Div = document.createElement('div');
+  Div.style.width = '80%';
+
+  Div.style.height = '250px';
+  Div.style.margin = '3% auto auto auto ';
+  Div.style.display = "flex"
+  Div.style.boxShadow = ' rgba(0, 0, 0, 0.35) 0px 5px 15px';
+  Div.style.padding = "1% 0"
+  Div.style.backgroundColor = '#d0ffd0';
+
+  
+    let imgDiv = document.createElement("img")
+    imgDiv.src = product.image;
+    imgDiv.style.width = "16vw"
+    imgDiv.style.margin = "0 0 0 8%"
+
+
+    let advantageDiv = document.createElement("div")
+    advantageDiv.style.width = "36%"
+    advantageDiv.style.margin = "0 0 0 3%"
+
+      let advText = document.createElement("div")
+      advText.innerText = "Advantages:"
+      advText.style.fontWeight = "600"
+      advText.style.fontSize = "1vw"
+      advText.style.margin = "1% 2% 0 0"
+
+      let advCont = document.createElement("div")
+      advCont.innerText = product.advantage
+      advCont.style.fontSize = ".9vw"
+      advCont.style.margin = "1% 0 0 0 "
+
     
-     let Div = document.createElement("div")
-     Div.style.width = "100%"
-     Div.style
+    advantageDiv.append(advText, advCont)
 
-})
+    let compositionDiv = document.createElement("div")
+    compositionDiv.style.width = "42%"
+    compositionDiv.style.margin = "0 0 0 5%"
+
+      let compText = document.createElement("div")
+      compText.textContent = "Compostition:"
+      compText.style.fontSize = "1vw"
+      compText.style.fontWeight = "600";
+      compText.style.margin = ".8% 0 0 0 "
+
+      let firstDiv = document.createElement("div")
+      firstDiv.textContent = product.composition1;
+      firstDiv.style.fontSize = ".9vw"
+      firstDiv.style.margin = "1.1% 0 0 0 "
+
+      let secondDiv = document.createElement('div');
+      secondDiv.textContent = product.composition2;
+      secondDiv.style.fontSize = '.9vw';
+      secondDiv.style.margin = '1.1% 0 0 0 ';
 
 
+      let thirdDiv = document.createElement('div');
+      thirdDiv.textContent = product.composition3;
+      thirdDiv.style.fontSize = '.9vw';
+      thirdDiv.style.margin = '1.1% 0 0 0 ';
 
 
+      let fourthDiv = document.createElement('div');
+      fourthDiv.textContent = product.composition4;
+      fourthDiv.style.fontSize = '.9vw';
+      fourthDiv.style.margin = '1.1% 0 0 0 ';
 
 
+      let fifthDiv = document.createElement('div');
+      fifthDiv.textContent = product.composition5;
+      fifthDiv.style.fontSize = '.9vw';
+      fifthDiv.style.margin = '1.1% 0 0 0 ';
+
+
+      let sixthDiv = document.createElement('div');
+      sixthDiv.textContent = product.composition6;
+      sixthDiv.style.fontSize = '.9vw';
+      sixthDiv.style.margin = '1.1% 0 0 0 ';
+
+
+    compositionDiv.append(compText, firstDiv, secondDiv, thirdDiv, fourthDiv, fifthDiv, sixthDiv)
+
+    Div.append(imgDiv, advantageDiv, compositionDiv)
+
+  main.append(Div);
+});
